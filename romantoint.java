3b5ae9af6100841f960dbcbaca7e001a61e5c7c5
Subbu64774 <subash.6471@gmail.com>
2016-06-14 00:13:42 -0700

    Create romantoint.java

---
+++ romantoint.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Subramanian Ganesan on 6/14/2016.
+ */
+public class romantoint {
+    //public static void main(String args[]){
+    public static String intToRoman(int num){
+            StringBuilder sb = new StringBuilder();
+            int times = 0;
+            String[] romans = new String[]{"I", "IV", "V", "IX", "X", "XL", "L",
+                    "XC", "C", "CD", "D", "CM", "M"};
+            int[] ints = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500,
+                    900, 1000};
+            for (int i = ints.length - 1; i >= 0; i--) {
+                times = num / ints[i];
+                num %= ints[i];
+                while (times > 0) {
+                    sb.append(romans[i]);
+                    times--;
+                }
+            }
+      //  }
+        return sb.toString();
+    }
+}
