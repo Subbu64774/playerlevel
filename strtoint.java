e6b467c556a04b9d752530d0c91f9ae89968baf7
Subbu64774 <subash.6471@gmail.com>
2016-06-13 23:57:32 -0700

    Create strtoint.java

---
+++ strtoint.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Subramanian Ganesan on 6/14/2016.
+ */
+public class strtoint
+{
+    public static void main(String[] args)
+    {
+        String numberAsString = "1234";
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("The number is: " + number);
+    }
+}
